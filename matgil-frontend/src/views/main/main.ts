efd17ab34b843aaa5eaad1abf7260efa007c8dce
JunyupHong <junyup.hong@gmail.com>
2020-06-15 00:20:24 +0900

    matgil FE 개발

--- matgil-frontend/src/views/main/main.ts
+++ matgil-frontend/src/views/main/main.ts
@@ -1,17 +1,40 @@
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import axios from 'axios';
+import _ from 'lodash';
 
 @Component({})
 export default class Main extends Vue {
+    private tabs: 'list' | 'info' = 'list';
     private selectedHighway = '';
     private selectedRestArea = '';
+    private highways = [];
+    private restAreas = [];
 
-    private highwayChange(value: string) {
+    private getTelFormat(tel: string) {
+        const telFormat = [...tel.split('')];
+        telFormat.reverse().splice(4, 0, '-');
+        telFormat.splice(8, 0, '-')
+        telFormat.reverse();
+        return telFormat.join('');
+    }
+    private async highwayChange(value: string) {
         console.log('고속도로 변경', value);
         this.selectedHighway = value;
+        this.tabs = 'list';
     }
     private restAreaChange(value: string) {
         console.log('휴게소 변경', value);
         this.selectedRestArea = value;
+        this.tabs = 'info';
+    }
+
+    private async clickHighway(value: any) {
+        console.log(value);
+        this.restAreas = (await axios(`http://127.0.0.1:3000/db/restarea/${value.hid}`)).data;
+        console.log(this.restAreas);
+    }
+    private async mounted() {
+        this.highways = (await axios.get('http://127.0.0.1:3000/db/highway')).data;
     }
 }
--- matgil-frontend/src/views/main/main.vue
+++ matgil-frontend/src/views/main/main.vue
@@ -8,7 +8,7 @@
             <div class="main-title small">따라</div>
         </div>
         <div class="content-zone">
-            <a-tabs default-active-key="list">
+            <a-tabs v-model="tabs">
                 <a-tab-pane key="list">
                     <span slot="tab">
                         <a-icon type="apple" />
@@ -17,7 +17,37 @@
                     <div v-if="selectedHighway === ''">
                         선택된 고속도로 없음
                     </div>
-                    <div v-else>{{ selectedHighway }}</div>
+                    <div v-else class="content-table-zone">
+                        <div class="content-table-title">
+                            {{ selectedHighway }}
+                        </div>
+                        <div class="content-table">
+                            <div class="attribute-zone">
+                                <div class="attribute flex-2">휴게소 이름</div>
+                                <div class="attribute flex-4">주소</div>
+                                <div class="attribute flex-2">전화번호</div>
+                                <div class="attribute flex-5">대표 음식</div>
+                                <!-- <div class="attribute flex-1">대형차</div>
+                                <div class="attribute flex-1">중형차</div>
+                                <div class="attribute flex-1">소형차</div> -->
+                            </div>
+                            <div class="tuple-zone">
+                                <div
+                                    v-for="(r, i) in restAreas"
+                                    :key="'tuple' + i"
+                                    class="tuple"
+                                >
+                                    <div class="item flex-2">{{ r.rname }}</div>
+                                    <div class="item flex-4">{{ r.address }}</div>
+                                    <div class="item flex-2">{{ getTelFormat(r.telephone) }}</div>
+                                    <div class="item flex-5">{{ r.fname.join(', ') }}</div>
+                                    <!-- <div class="item flex-1">{{  }}</div>
+                                    <div class="item flex-1">11</div>
+                                    <div class="item flex-1">11</div> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </a-tab-pane>
                 <a-tab-pane key="info" tab="휴게소 정보" force-render>
                     <div v-if="selectedRestArea === ''">
@@ -29,33 +59,32 @@
             <div class="select-zone">
                 <a-select
                     default-value="고속도로 선택"
-                    style="width: 120px"
+                    style="width: 200px"
                     @change="highwayChange"
+                    v-if="highways.length !== 0"
                 >
-                    <a-select-option value="Jack">
-                        Jack
-                    </a-select-option>
-                    <a-select-option value="lucy">
-                        Lucy
-                    </a-select-option>
-                    <a-select-option value="Yiminghe">
-                        yiminghe
+                    <a-select-option
+                        v-for="h in highways"
+                        :key="h.routeCd"
+                        :value="h.hname + '(' + h.gudclssnm + ')'"
+                        @click="clickHighway(h)"
+                    >
+                        {{ h.hname + '(' + h.gudclssnm + ')' }}
                     </a-select-option>
                 </a-select>
                 <div style="width: 20px"></div>
                 <a-select
                     default-value="휴게소 선택"
-                    style="width: 120px"
+                    style="width: 200px"
                     @change="restAreaChange"
+                    v-if="restAreas.length !== 0"
                 >
-                    <a-select-option value="Jack">
-                        Jack
-                    </a-select-option>
-                    <a-select-option value="lucy">
-                        Lucy
-                    </a-select-option>
-                    <a-select-option value="Yiminghe">
-                        yiminghe
+                    <a-select-option
+                        v-for="r in restAreas"
+                        :key="r.rid"
+                        :value="r.rname"
+                    >
+                        {{ r.rname }}
                     </a-select-option>
                 </a-select>
             </div>
