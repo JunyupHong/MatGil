5073e46735e8c0e0e877882721297a7c581699eb
JunyupHong <junyup.hong@gmail.com>
2020-06-21 00:43:07 +0900

    matgil server complete

--- matgil-server/app.js
+++ matgil-server/app.js
@@ -51,104 +51,94 @@ app.use(function(err, req, res, next) {
 const { Client } = require('pg');
 
 // DB initialize
-// (async () => {
-//   let client;
-//   try {
+(async () => {
+  let client;
+  try {
 
-//     client = new Client({
-//       user : 'junyup',
-//       host : '127.0.0.1',
-//       database : 'postgres',
-//       password : 'hong082121`',
-//       port : 5432,
-//     });
-//     await client.connect();
-//     await client.query(`
-//       DROP TABLE Brand;
-//       DROP TABLE Convenience;
-//       DROP TABLE Food;
-//       DROP TABLE RestArea;
-//       DROP TABLE highway;
-//     `);
-//     console.log('[COMPLETE] DROP ALL TABLE');
+    client = new Client({
+      user : 'junyup',
+      host : '127.0.0.1',
+      database : 'postgres',
+      port : 5432,
+    });
+    await client.connect();
+    await client.query(`
+      DROP TABLE Brand;
+      DROP TABLE Convenience;
+      DROP TABLE Food;
+      DROP TABLE RestArea;
+      DROP TABLE highway;
+    `);
+    console.log('[COMPLETE] DROP ALL TABLE');
     
-//     await client.query(`
-//       CREATE TABLE Highway (hID int, hName varchar(20), gudClssCd int, gudClssNm varchar(5), hdqrCd int, hdqrNm varchar(20), mtnofCd int, mtnofNM varchar(20), primary key(hID));
-//       CREATE TABLE RestArea (rID int, rName varchar(20), address varchar(100), telephone varchar(20), cocrPrkgTrcn varchar(20), fscarPrkgTrcn varchar(20), dspnPrkgTrcn varchar(20), hID int, primary key(rID), foreign key(hID) references Highway(hID));
-//       CREATE TABLE Food (fName varchar(50), fCost int, recommendMenu varchar(3), seasonMenu varchar(3), bestMenu varchar(3), premiumMenu varchar(3), foodMaterial varchar(300), rID int, foreign key(rID) references RestArea(rID));
-//       CREATE TABLE Convenience (cName varchar(20), rID int, cDesc varchar(200), foreign key(rID) references RestArea(rID));
-//       CREATE TABLE Brand (bName varchar(20), rID int, bDesc varchar(200), foreign key(rID) references RestArea(rID));
-//     `);
-//     console.log('[COMPLETE] CREATE ALL TABLE');
+    await client.query(`
+      CREATE TABLE Highway (hID int, hName varchar(20), gudClssCd int, gudClssNm varchar(5), hdqrCd int, hdqrNm varchar(20), mtnofCd int, mtnofNM varchar(20), primary key(hID));
+      CREATE TABLE RestArea (rID int, rName varchar(20), address varchar(100), telephone varchar(20), cocrPrkgTrcn varchar(20), fscarPrkgTrcn varchar(20), dspnPrkgTrcn varchar(20), hID int, primary key(rID), foreign key(hID) references Highway(hID));
+      CREATE TABLE Food (fID int, fName varchar(50), fCost int, fDesc varchar(500), recommendMenu varchar(3), seasonMenu varchar(3), bestMenu varchar(3), premiumMenu varchar(3), foodMaterial varchar(300), rID int, foreign key(rID) references RestArea(rID));
+      CREATE TABLE Convenience (cID int, cName varchar(20), rID int, cDesc varchar(200), foreign key(rID) references RestArea(rID));
+      CREATE TABLE Brand (bID int, bName varchar(20), rID int, bDesc varchar(200), foreign key(rID) references RestArea(rID));
+    `);
+    console.log('[COMPLETE] CREATE ALL TABLE');
 
-//     /* 휴게소+주유소 정보 -> RestArea Table */
-//     let req = (await axios.get('http://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList?key=3357093317&type=json')).data.list;
+    /* 휴게소+주유소 정보 -> RestArea Table */
+    let req = (await axios.get('http://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList?key=3357093317&type=json')).data.list;
     
-//     /* 고속도로 정보 정보 -> Highway Table */
-//     const highwayData = _.uniqBy(req, d => d.routeCd + d.gudClssCd + '');
-//     for (let i = 0; i < highwayData.length; i++) {
-//       const hid = highwayData[i].routeCd + highwayData[i].gudClssCd + '';
-//       await client.query(`INSERT INTO highway (hID, hName, gudClssCd, gudClssNm, hdqrCd, hdqrNm, mtnofCd, mtnofNM) VALUES (${hid*1},'${highwayData[i].routeNm}',${highwayData[i].gudClssCd},'${highwayData[i].gudClssNm}',${highwayData[i].hdqrCd},'${highwayData[i].hdqrNm}',${highwayData[i].mtnofCd}, '${highwayData[i].mtnofNm}');`);
-//     }
-//     console.log('[COMPLETE] INIT Highway Table');
+    /* 고속도로 정보 정보 -> Highway Table */
+    const highwayData = _.uniqBy(req, d => d.routeCd + d.gudClssCd + '');
+    for (let i = 0; i < highwayData.length; i++) {
+      const hid = highwayData[i].routeCd + highwayData[i].gudClssCd + '';
+      await client.query(`INSERT INTO highway (hID, hName, gudClssCd, gudClssNm, hdqrCd, hdqrNm, mtnofCd, mtnofNM) VALUES (${hid*1},'${highwayData[i].routeNm}',${highwayData[i].gudClssCd},'${highwayData[i].gudClssNm}',${highwayData[i].hdqrCd},'${highwayData[i].hdqrNm}',${highwayData[i].mtnofCd}, '${highwayData[i].mtnofNm}');`);
+    }
+    console.log(`[COMPLETE] INIT Highway Table - ${highwayData.length} tuples`);
 
-//     for (let i = 0; i < req.length; i++) {
-//       const hid = req[i].routeCd + req[i].gudClssCd + '';
-//       await client.query(`INSERT INTO restarea (rID, rName, address, telephone, cocrPrkgTrcn, fscarPrkgTrcn, dspnPrkgTrcn, hID) VALUES (${req[i].svarCd}, '${req[i].svarNm}', '${req[i].svarAddr}', '${req[i].rprsTelNo}', '${req[i].cocrPrkgTrcn}', '${req[i].fscarPrkgTrcn}', '${req[i].dspnPrkgTrcn}', ${hid});`)
-//     }
-//     console.log('[COMPLETE] INIT Restarea Table');
-    
-//     // // 휴게소 정보
-//     // const restAreaData = _.filter(req, d => d.svarGsstClssNm === '휴게소');
-//     // // 주유소 정보
-//     // const gasStationData = _.filter(req, d => d.svarGsstClssNm === '주유소');
-//     // for (let i = 0; i < restAreaData.length; i++) {
-//     //   await client.query(`INSERT INTO restarea (rID, rName, address, telephone, cocrPrkgTrcn, fscarPrkgTrcn, dspnPrkgTrcn, hID) VALUES (${restAreaData[i].svarCd}, '${restAreaData[i].svarNm}', '${restAreaData[i].svarAddr}', '${restAreaData[i].rprsTelNo}', '${restAreaData[i].cocrPrkgTrcn}', '${restAreaData[i].fscarPrkgTrcn}', '${restAreaData[i].dspnPrkgTrcn}', ${restAreaData[i].routeCd});`)
-//     // }
+    for (let i = 0; i < req.length; i++) {
+      const hid = req[i].routeCd + req[i].gudClssCd + '';
+      await client.query(`INSERT INTO restarea (rID, rName, address, telephone, cocrPrkgTrcn, fscarPrkgTrcn, dspnPrkgTrcn, hID) VALUES (${req[i].svarCd}, '${req[i].svarNm}', '${req[i].svarAddr}', '${req[i].rprsTelNo}', '${req[i].cocrPrkgTrcn}', '${req[i].fscarPrkgTrcn}', '${req[i].dspnPrkgTrcn}', ${hid});`)
+    }
+    console.log(`[COMPLETE] INIT Restarea Table - ${req.length} tuples`);
+
     
     
-//     /* 휴게시설 정보 -> Convenience Table */
-//       // 주유소의 휴게시설
-//     let convenienceData = (await axios.get('http://data.ex.co.kr/openapi/restinfo/restOilList?key=3357093317&type=json')).data.list;
-//       // 휴게소의 휴게시설
-//     for (let i = 1; i <= 10; i++) {
-//       convenienceData = convenienceData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restConvList?key=3357093317&type=json&numOfRows=99&pageNo=${i}`)).data.list);
-//     }
-//     for (let i = 0; i < convenienceData.length; i++) {
-//       // rID === 900458 인 이상한 친구가 있어서 거름
-//       if (convenienceData[i].stdRestNm === null) continue;
-//       await client.query(`INSERT INTO Convenience (cName, rID, cDesc) VALUES ('${convenienceData[i].psName}', ${convenienceData[i].stdRestCd}, '${convenienceData[i].psDesc !== null ? convenienceData[i].psDesc.replace(/[\'\;]/g, ' ') : "-"}');`);
-//     }
-//     console.log('[COMPLETE] INIT Convenience Table');
+    /* 휴게시설 정보 -> Convenience Table */
+      // 주유소의 휴게시설
+    let convenienceData = (await axios.get('http://data.ex.co.kr/openapi/restinfo/restOilList?key=3357093317&type=json')).data.list;
+      // 휴게소의 휴게시설
+    for (let i = 1; i <= 10; i++) {
+      convenienceData = convenienceData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restConvList?key=3357093317&type=json&numOfRows=99&pageNo=${i}`)).data.list);
+    }
+    for (let i = 0; i < convenienceData.length; i++) {
+      // rID === 900458 인 이상한 친구가 있어서 거름
+      if (convenienceData[i].stdRestNm === null) continue;
+      await client.query(`INSERT INTO Convenience (cID, cName, rID, cDesc) VALUES (${convenienceData[i].psCode}, '${convenienceData[i].psName}', ${convenienceData[i].stdRestCd}, '${convenienceData[i].psDesc !== null ? convenienceData[i].psDesc.replace(/[\'\;]/g, ' ') : "-"}');`);
+    }
+    console.log(`[COMPLETE] INIT Convenience Table - ${convenienceData.length - 1} tuples`);
 
-//     /* 음식 정보 -> Food Table */
-//     let foodData = [];
-//     for (let i = 1; i <= 35; i++) {
-//       foodData = foodData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restBestfoodList?key=test&type=json&numOfRows=99&pageNo=${i}`)).data.list);
-//     }
-//     for (let i = 0; i < foodData.length; i++) {
-//       await client.query(`INSERT INTO Food (fName, fCost, recommendMenu, seasonMenu, bestMenu, premiumMenu, foodMaterial, rID) VALUES ('${foodData[i].foodNm}', ${foodData[i].foodCost}, '${foodData[i].recommendyn}', '${foodData[i].seasonMenu}', '${foodData[i].bestfoodyn}', '${foodData[i].premiumyn}', '${foodData[i].foodMaterial !== null ? foodData[i].foodMaterial.replace(/[\'\;]/g, ' ') : "-"}', ${foodData[i].stdRestCd});`);
-//     }
-//     console.log('[COMPLETE] INIT Food Table');
+    /* 음식 정보 -> Food Table */
+    let foodData = [];
+    for (let i = 1; i <= 35; i++) {
+      foodData = foodData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restBestfoodList?key=test&type=json&numOfRows=99&pageNo=${i}`)).data.list);
+    }
+    for (let i = 0; i < foodData.length; i++) {
+      await client.query(`INSERT INTO Food (fID, fName, fCost, fDesc, recommendMenu, seasonMenu, bestMenu, premiumMenu, foodMaterial, rID) VALUES (${foodData[i].seq}, '${foodData[i].foodNm}', ${foodData[i].foodCost}, '${foodData[i].etc !== null ? foodData[i].etc.replace(/[\'\;\"]/g, ' ') : '-'}', '${foodData[i].recommendyn}', '${foodData[i].seasonMenu}', '${foodData[i].bestfoodyn}', '${foodData[i].premiumyn}', '${foodData[i].foodMaterial !== null ? foodData[i].foodMaterial.replace(/[\'\;]/g, ' ') : "-"}', ${foodData[i].stdRestCd});`);
+    }
+    console.log(`[COMPLETE] INIT Food Table - ${foodData.length} tuples`);
 
 
-//     /* 브랜드 정보 -> Brand Table */
-//     let brandData = [];
-//     for (let i = 1; i <= 7; i++) {
-//       brandData = brandData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restBrandList?key=test&type=json&numOfRows=99&pageNo=${i}`)).data.list);
-//     }
-//     for (let i = 0; i < brandData.length; i++) {
-//       await client.query(`INSERT INTO Brand (bName, rID, bDesc) VALUES ('${brandData[i].brdName}', ${brandData[i].stdRestCd}, '${brandData[i].brdDesc !== null ? brandData[i].brdDesc.replace(/[\'\;]/g, " ") : "-"}');`);
-//     }
-//     console.log('[COMPLETE] INIT Brand Table');
+    /* 브랜드 정보 -> Brand Table */
+    let brandData = [];
+    for (let i = 1; i <= 7; i++) {
+      brandData = brandData.concat((await axios.get(`http://data.ex.co.kr/openapi/restinfo/restBrandList?key=test&type=json&numOfRows=99&pageNo=${i}`)).data.list);
+    }
+    for (let i = 0; i < brandData.length; i++) {
+      await client.query(`INSERT INTO Brand (bID, bName, rID, bDesc) VALUES (${brandData[i].brdCode}, '${brandData[i].brdName}', ${brandData[i].stdRestCd}, '${brandData[i].brdDesc !== null ? brandData[i].brdDesc.replace(/[\'\;]/g, " ") : "-"}');`);
+    }
+    console.log(`[COMPLETE] INIT Brand Table - ${brandData.length} tuples`);
     
-//     console.log('done');
-//   } catch(e) {
-//     console.error(e);
-//     console.log('error');
-//   }
-//   // client.end();
-// })();
+    console.log('done');
+  } catch(e) {
+    console.error(e);
+  }
+})();
 
 
 
--- matgil-server/routes/index.js
+++ matgil-server/routes/index.js
@@ -7,37 +7,39 @@ const client = new Client({
         user : 'junyup',
         host : '127.0.0.1',
         database : 'postgres',
-        password : 'hong082121`',
         port : 5432,
       });
 
 (async () => {
   await client.connect();
 })();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/food', function(req, res, next) {
-  
-});
 
-router.get('/highway', async (req, res) => {
-  
+// 전체 고속도로 데이터
+router.get('/highway', async (req, res) => {  
   const result = (await client.query(`SELECT * from highway`)).rows;
+  console.log(`[QUERY] SELECT * FROM highway`);
+  console.log(`  [RESULT] 전체 고속도로 데이터: ${result.length} 개`);
   res.send(result);
 });
 
+// 선택된 고속도로 데이터
 router.get('/highway/:hid', async (req, res) => {
   const result = (await client.query(`SELECT * from highway where hid=${req.params.hid}`)).rows;
+  console.log(`[QUERY] SELECT * from highway where hid=${req.params.hid}`);
+  console.log(`  [RESULT] 선택된 고속도로 데이터: ${result.length} 개`);
   res.send(result);
 });
+
+// 고속도로 상의 휴게소 리스트 및 정보
 router.get('/restarea/:hid', async (req, res) => {
-  // const result = (await client.query(`SELECT * FROM restarea WHERE hID=${req.params.hid}`)).rows;
   const all = (await client.query(`SELECT *
-    FROM RestArea natural join Food 
-    WHERE restarea.hID=${req.params.hid};`)).rows;
+    FROM RestArea natural full outer join food 
+    WHERE restarea.hID=${req.params.hid}
+    order by rid asc;`)).rows;
+  
+  console.log(`[QUERY] SELECT * FROM RestArea natural full outer join food WHERE restarea.hID=${req.params.hid} order by rid asc;`);
+  console.log(`  [RESULT] 선택된 고속도로 상의 휴게소 리스트 및 정보: ${all.length} 개`);
   const result = [];
   all.forEach(ele => {
     const idx = _.findIndex(result, r => r.rid === ele.rid);
@@ -47,17 +49,82 @@ router.get('/restarea/:hid', async (req, res) => {
       result[idx].fname.push(ele.fname);
     }
   });
+  
   res.send(result);
 });
 
-// router.get('/restarea/:hid', async (req, res) => {
-//   const result = (await client.query(`SELECT distinct *
-//     FROM RestArea, Food, highway
-//     WHERE HIGHWAY.hID=${req.params.hid} and food.bestmenu='Y' and highway.gudclsscd = ${req.params.gudclsscd}
-//     order by rname asc;`)).rows;
-//   res.send(result);
-// });
 
+router.get('/restarea/info/:rid', async (req, res) => {
+  const all = (await client.query(`
+    SELECT Restarea.rid, rName,
+      fid, fname, fcost,
+      null ::integer as bid, null as bname, null as bdesc,
+      null ::integer as cid, null as cname, null as cdesc
+    FROM Food natural join RestArea
+    WHERE Food.rID=${req.params.rid}
+    union
+    SELECT Restarea.rid, rName,
+      null, null, null,
+      bid, bname, bdesc,
+      null, null, null
+    FROM Brand natural join RestArea
+    WHERE brand.rID=${req.params.rid}
+    union
+    SELECT Restarea.rid, rname,
+      null, null, null,
+      null, null, null,
+      cid, cname, cdesc
+    FROM Convenience natural join RestArea
+    WHERE Convenience.rID=${req.params.rid}
+  `)).rows;
 
+  console.log(`SELECT Restarea.rid, rName, fid, fname, fcost, null ::integer as bid, null as bname, null as bdesc, null ::integer as cid, null as cname, null as cdesc FROM Food natural join RestArea WHERE Food.rID=${req.params.rid}
+    union
+    SELECT Restarea.rid, rName, null, null, null, bid, bname, bdesc, null, null, null FROM Brand natural join RestArea WHERE brand.rID=${req.params.rid}
+    union
+    SELECT Restarea.rid, rname, null, null, null, null, null, null, cid, cname, cdesc FROM Convenience natural join RestArea WHERE Convenience.rID=${req.params.rid}`);
+  console.log(`  [RESULT] 선택된 휴게소 정보 (음식, 브랜드, 편의시설): ${all.length} 개`);
+
+  const result = {
+    restAreaData: {},
+    food: [],
+    brand: [],
+    convenience: [],
+  };
+
+  _.forEach(all, (data, i) => {
+    if (i === 0) {
+      result.restAreaData.rid = data.rid;
+      result.restAreaData.rname = data.rname;
+    }
+    if (data.fname) {
+      result.food.push({
+        fid: data.fid,
+        fname: data.fname,
+        fcost: data.fcost,
+      });
+    }
+    if (data.bname) result.brand.push({
+      bid: data.bid,
+      bname: data.bname,
+      bdesc: data.bdesc
+    });
+    if (data.cname) result.convenience.push({
+      cid: data.cid,
+      cname: data.cname,
+      cdesc: data.cdesc
+    });
+  });
+  res.send(result);
+});
+
+router.get('/food/:fid', async (req, res) => {
+  const result = (await client.query(`SELECT * from FOOD WHERE fid=${req.params.fid}`)).rows;
+  
+  console.log(`[QUERY] SELECT * FROM RestArea natural full outer join food WHERE restarea.hID=${req.params.hid} order by rid asc;`);
+  console.log(`  [RESULT] 선택된 음식 정보: ${ result.length } 개`);
+  console.log(result[0]);
+  res.send(result[0]);
+});
 
 module.exports = router;
